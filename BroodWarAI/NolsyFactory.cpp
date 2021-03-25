b58a1e68fb25c84fdc9a0c7a16bd75aff3dc951a
Tyler Beaupre <tbeaupre@wpi.edu>
2021-03-25 15:42:16 -0400

    NolsyFactory now returns the correct type of Nolsy for Units/Structures

--- BroodWarAI/NolsyFactory.cpp
+++ BroodWarAI/NolsyFactory.cpp
@@ -8,6 +8,7 @@
 using namespace BuildActionEnums;
 
 NolsyBase *NolsyFactory::BuildNolsy(const BuildAction *action) {
+	BWAPI::Broodwar << "Building a Nolsy: " << action->PrintAction() << std::endl;
 	switch (action->getType()) {
 		case ActionType::OTHER:
 			return new NolsyOther(action->getOtherType());
@@ -15,9 +16,9 @@ NolsyBase *NolsyFactory::BuildNolsy(const BuildAction *action) {
 			return new NolsyTech(*action->getTechType());
 		case ActionType::UNIT:
 			if (action->isBuilding()) {
-				return new NolsyUnit(*action->getUnitType());
-			} else {
 				return new NolsyStructure(*action->getUnitType());
+			} else {
+				return new NolsyUnit(*action->getUnitType());
 			}
 		case ActionType::UPGRADE:
 			return new NolsyUpgrade(*action->getUpgradeType());
